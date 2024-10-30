213647e3efe8d60e5d765d88e47a6277b0a1e089
Abdyjaparov <159744880+Abdyjaparov@users.noreply.github.com>
2024-10-30 14:58:06 +0600

    uigyhjh,
    hk,gk,gku,g

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,11 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <p className='p1'>© 2024 Argen Abdyjaparov. All rights reserved.</p>
-            <div className="footer-links">
-                <a href="https://github.com/Abdyjaparov" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="/Contact">Contact</a>
-                
-            </div>
+           
             <p className='made-man'>Made by man from the earth. </p>
         </footer>
     );
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,14 +2,7 @@ import React, { useState, useEffect } from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Импортируем Font Awesome
 import '../Style/Contact.css';
 
-const Banner = ({ message, onClose }) => {
-    return (
-        <div className="banner">
-            {message}
-            <button onClick={onClose}>Close</button>
-        </div>
-    );
-};
+
 
 const Contact = () => {
     const [showBanner, setShowBanner] = useState(false);
@@ -35,7 +28,7 @@ const Contact = () => {
 
     return (
         <main style={{ padding: '20px' }}>
-            {showBanner && <Banner message="Welcome to the Contact Page!" onClose={handleCloseBanner} />}
+           
             <div className="contact-info" onClick={() => copyToClipboard("+996998444307")}>
                 <h2><i className="fas fa-phone"></i> +996998444307</h2> 
             </div>
