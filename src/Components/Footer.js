8f4b0a43e9b0a5536c9e3fb802b88b04a947613a
Abdyjaparov <159744880+Abdyjaparov@users.noreply.github.com>
2024-10-30 22:23:22 +0600

    commit
    i love you

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
     return (
         <footer className="footer">
             <p className='p1'>© 2024 Argen Abdyjaparov. All rights reserved.</p>
-           
+            <div className="footer-links">
+                <a href="https://github.com/Abdyjaparov" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a href="/contact">Contact</a>
+            </div>
             <p className='made-man'>Made by man from the earth. </p>
         </footer>
     );
